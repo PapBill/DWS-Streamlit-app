2298a2fb69a90a33f5c0a47a5a89b4b5881bff8e
PapBill <82447531+PapBill@users.noreply.github.com>
2026-03-21 01:26:42 +0200

    Update Streamlit app description

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.title("🏚 Airbnb Project")
 
-st.write("ABC")
+st.write("       My Streamlit app for Aibnb data visualization and price prediction")
 
 with st.expander("Dataset Overview"):
     df = pd.read_csv('https://raw.githubusercontent.com/PapBill/DWS-Streamlit-app/main/dataset.csv', encoding='latin1')
