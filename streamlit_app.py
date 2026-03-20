55545252464a25cf55a7ffa14ce37df478628ead
PapBill <82447531+PapBill@users.noreply.github.com>
2026-03-21 01:21:23 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ st.title("🏚 Airbnb Project")
 st.write("SFcdcsaccsc")
 
 with st.expander("Dataset Overview"):
-
- df =pd.read_csv('https://github.com/PapBill/DWS-Streamlit-app/blob/main/dataset.csv')
- st.write("Neapoli Dataset")
- df
+    df = pd.read_csv('https://raw.githubusercontent.com/PapBill/DWS-Streamlit-app/main/dataset.csv')
+    st.write("Neapoli Dataset")
+    st.write(df)
